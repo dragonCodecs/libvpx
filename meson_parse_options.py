6f4067f01291ad52ee5551be425b57997321abc3
L. E. Segovia <amy@amyspark.me>
2023-04-01 22:31:33 -0300

    meson: Fix option parsing and generation and prepare for processing

--- meson_parse_options.py
+++ meson_parse_options.py
@@ -4,8 +4,7 @@
 # SPDX-License-Identifier: LGPL-2.1-or-later
 
 import re
-import subprocess
-from warnings import warn, WarningMessage
+from warnings import warn
 
 def parse_options(lines: list[str]) -> dict:
     current_options: dict = {}
@@ -63,7 +62,6 @@ def parse_options(lines: list[str]) -> dict:
         if option:
             current_options[option.group(1).lower().replace('-', '_')] = {
                 'type': 'string',
-                'value': '',
                 'description': option.group(2)
             }
         elif line.startswith('--') and not re.match(r'.+\<[a-z]+\>.*', line):
@@ -99,21 +97,21 @@ def filter_meson_handled_options(pair) -> bool:
 def update_meson_options(options: dict):
     has_generated = False
     lines = []
-    with open('meson_options.txt', 'r', encoding='utf8') as meson_file:
+    with open('meson_options.txt', 'r', encoding='utf-8') as meson_file:
         opening = '#### --- GENERATED EXTERN OPTIONS --- ####\n'
         closing = opening.replace('GENERATED', 'END GENERATED')
         for l in meson_file.readlines():
             if l == opening:
                 has_generated = True
                 lines.append(l)
-                for key, kv in dict(filter(filter_meson_handled_options, options.items())).items():
+                for key, kv in options.items():
                     if kv['type'] == 'combo':
                         choices = ', '.join([f"'{i}'" for i in kv['choices']])
                         lines.append(f"option('{key}', type: 'combo', choices: [{choices}], description: '{kv['description']}')\n")
                     elif kv['type'] == 'boolean':
                         lines.append(f"option('{key}', type: 'boolean', value: {kv['value']}, description: '{kv['description']}')\n")
                     else:
-                        lines.append(f"option('{key}', type: 'string', value: '{kv['value']}', description: '{kv['description']}')\n")
+                        lines.append(f"option('{key}', type: 'string', {"value: '{kv['value']}', " if kv['value']}, description: '{kv['description']}')\n")
             elif l == closing:
                 lines.append(l)
                 has_generated = False
@@ -123,8 +121,39 @@ def update_meson_options(options: dict):
     with open('meson_options.txt', 'w') as meson_file:
         meson_file.write(''.join(lines))
 
+def update_meson_build(options: dict):
+    has_generated = False
+    lines = []
+    booleans = [i for i in options.keys() if options.get(i)['type'] == 'boolean']
+    with open('meson.build', 'r', encoding='utf-8') as meson_file:
+        opening = '#### --- GENERATED EXTERN OPTIONS --- ####\n'
+        closing = opening.replace('GENERATED', 'END GENERATED')
+        for l in meson_file.readlines():
+            if l == opening:
+                has_generated = True
+                lines.append(l)
+                lines.append('IGNORE_MESON_BUILTINS = [\n')
+                for i in MESON_HANDLED_OPTIONS:
+                    lines.append(f"\t'{i}',\n")
+                lines.append(']\n')
+                lines.append('\n')
+                lines.append('MESON_BOOLEAN_OPTIONS = [\n')
+                for i in booleans:
+                    lines.append(f"\t'{i}',\n")
+                lines.append(']\n')
+            elif l == closing:
+                lines.append(l)
+                has_generated = False
+            elif not has_generated:
+                lines.append(l)
+
+    with open('meson.build', 'w') as meson_file:
+        meson_file.write(''.join(lines))
+
 if __name__=='__main__':
     # ./configure --help > tmp_meson_options.txt
     with open('tmp_meson_options.txt', 'r', encoding='utf-8') as f:
         options = parse_options(f.readlines())
+        options = dict(filter(filter_meson_handled_options, options.items()))
         update_meson_options(options)
+        update_meson_build(options)
