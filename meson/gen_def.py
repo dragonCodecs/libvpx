4405d52274528b21a76a3aeb5f3412b239ca1583
L. E. Segovia <amy@amyspark.me>
2023-04-06 22:51:57 +0000

    meson: Add exports file generation

---
+++ meson/gen_def.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+
+# SPDX-FileCopyrightText: 2023 L. E. Segovia <amy@amyspark.me>
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
+from argparse import ArgumentParser, FileType, REMAINDER
+from io import StringIO, TextIOWrapper
+from sys import platform
+import re
+from sys import stdout
+
+TEXT_SYMBOL = r'^text\s+(.+)$'
+DATA_SYMBOL = r'^data\s+(.+)$'
+COMMENT = r'^#(.+)'
+
+# Unlike the stock generator script, this one does not accept 'name'
+# as that tells LINK.EXE to change the library export file name.
+def collate_windows_exports(files: TextIOWrapper, needs_underscore: bool) -> list[str]:
+    lines = ['EXPORTS']
+
+    for file in files:
+        for symbol in file.readlines():
+            symbol = symbol.strip()
+            if re.match(TEXT_SYMBOL, symbol):
+                lines += f"\t{'_' if needs_underscore else ''}{symbol.rsplit(' ', 1)[0]}"
+            elif re.match(DATA_SYMBOL, symbol):
+                lines += f"\t{'_' if needs_underscore else ''}{symbol.rsplit(' ', 1)[0]}\tDATA"
+            elif re.match(COMMENT, symbol):
+                continue
+            else:
+                raise RuntimeError(f'Unknown line: {symbol}')
+
+    return lines
+
+def collate_linux_ver(files: TextIOWrapper, needs_underscore: bool) -> list[str]:
+    lines = ['{ global:']
+    for file in files:
+        for symbol in file.readlines():
+            symbol = symbol.strip()
+            if re.match(TEXT_SYMBOL, symbol) or re.match(DATA_SYMBOL, symbol):
+                lines += f"{'_' if needs_underscore else ''}{symbol.rsplit(' ', 1)[0]};"
+            elif re.match(COMMENT, symbol):
+                continue
+            else:
+                raise RuntimeError(f'Unknown line: {symbol}')
+
+    lines += ['local: *; };']
+
+    return lines
+
+def collate_macos_sym(files: TextIOWrapper, needs_underscore: bool) -> list[str]:
+    lines = []
+    for file in files:
+        for symbol in file.readlines():
+            symbol = symbol.strip()
+            if re.match(TEXT_SYMBOL, symbol) or re.match(DATA_SYMBOL, symbol):
+                lines += f"{'_' if needs_underscore else ''}{symbol.rsplit(' ', 1)[0]}"
+            elif re.match(COMMENT, symbol):
+                continue
+            else:
+                raise RuntimeError(f'Unknown line: {symbol}')
+
+    return lines
+
+def collate_exports(files: TextIOWrapper, format='win', needs_underscore=False) -> list[str]:
+    if format == 'mac':
+        return collate_macos_sym(files, needs_underscore)
+    elif format == 'linux':
+        return collate_linux_ver(files, needs_underscore)
+    else:
+        return collate_windows_exports(files, needs_underscore)
+
+if __name__ == '__main__':
+    parser = ArgumentParser(description='''
+        This script generates a module definition file containing a list of symbols
+        to export from a library. Source files are technically bash scripts (and thus may
+        use #comment syntax) but in general, take the form of a list of symbols:
+
+          <kind> symbol1 [symbol2, symbol3, ...]
+
+        where <kind> is either 'text' or 'data'
+        ''')
+    parser.add_argument('--out', type=FileType('w', encoding='utf-8'), default=stdout, help='Write output to a file')
+    parser.add_argument('--name', type=str, help='Name of the library (required)')
+    parser.add_argument('--format', type=str, choices=['win', 'mac', 'linux'], default='win', help='Format of the module definition file')
+    parser.add_argument('--underscore', action='store_true', help='The symbols need an underscore for exporting')
+    parser.add_argument('files', nargs=REMAINDER, type=FileType('r', encoding='utf-8'))
+    args = parser.parse_args()
+
+    lines = collate_exports(args.files, format=args.format, needs_underscore=args.underscore)
+
+    tmp = StringIO()
+
+    for line in lines:
+        tmp.write(line)
+        tmp.write('\n')
+
+    args.out.write(tmp.getvalue())
