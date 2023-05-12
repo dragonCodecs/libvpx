f813b8c11a491665efe154a860290f347de6ad2e
L. E. Segovia <amy@amyspark.me>
2023-05-12 13:45:06 -0300

    meson: Fix IO wrapper not passing arguments adequately

--- meson/stdinout_wrapper.py
+++ meson/stdinout_wrapper.py
@@ -9,11 +9,11 @@ import subprocess
 
 if __name__ == '__main__':
 	parser = ArgumentParser(description='Redirect I/O streams from/to the given files')
-	parser.add_argument('--input', type=FileType('r', encoding='utf-8'), help='Pipe this file to STDIN')
+	parser.add_argument('--input', type=FileType('rb'), help='Pipe this file to STDIN')
 	parser.add_argument('--output', type=FileType('w', encoding='utf-8'), help='Pipe STDOUT to this file')
 	parser.add_argument('executable', help='Executable to run')
 	parser.add_argument('args', nargs=REMAINDER, help='Arguments')
 
 	args = parser.parse_args()
 
-	subprocess.run(args.executable, args.args, input=args.input, stdout=args.output, check=True)
+	subprocess.run([args.executable] + args.args, input=args.input.read(), stdout=args.output, check=True)
